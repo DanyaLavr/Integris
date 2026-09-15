ce450c92334fd2df3fcd5202ea9998bedb7495fd
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2026-09-15 12:23:14 +0200

    fix: fix lines of burger menu on tablets

--- src/shared/ui/BurgerMenu.tsx
+++ src/shared/ui/BurgerMenu.tsx
@@ -9,10 +9,10 @@ const BurgerMenu = ({ isOpen, setIsOpen }: IProps) => {
       onClick={setIsOpen}
     >
       <span
-        className={`block w-6 h-1 bg-stone-900 rounded-2xl sm:w-10 sm:h-1.5 transition ${isOpen ? "-rotate-45 translate-y-1" : ""}`}
+        className={`block w-6 h-1 bg-stone-900 rounded-2xl sm:w-10 sm:h-1.5 transition ${isOpen ? "-rotate-45 translate-y-1 sm:translate-y-2.5" : ""}`}
       ></span>
       <span
-        className={`block w-6 h-1 bg-stone-900 rounded-2xl sm:w-10 sm:h-1.5 transition ${isOpen ? "rotate-45 -translate-y-0.5" : ""}`}
+        className={`block w-6 h-1 bg-stone-900 rounded-2xl sm:w-10 sm:h-1.5 transition ${isOpen ? "rotate-45 -translate-y-0.5 sm:-translate-y-0" : ""}`}
       ></span>
       <span
         className={`block w-6 h-1 bg-stone-900 rounded-2xl sm:w-10 sm:h-1.5 transition ${isOpen ? "rotate-45 -translate-y-2 opacity-0" : ""}`}
